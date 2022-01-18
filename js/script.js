89bacad4278e668fae98071278cf096356a04867
mo <mohibullah.kamal@gmail.com>
2022-01-18 16:31:48 +0600

    add log

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('Hello World...');
+let a = 1;
+let b = 2;
+let c = 3;
+
+console.log(a);
+console.log(b);
+console.log(c);
+a = c;
+console.log(a);
+
+//console.log('Hello World... Just a test')
+
+
+
