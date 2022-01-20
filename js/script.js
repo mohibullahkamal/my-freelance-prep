a08c6ecaffafc844685b28e562af8430c09694ec
mo <mohibullah.kamal@gmail.com>
2022-01-20 10:02:31 +0600

    add parameters to callback

--- js/script.js
+++ js/script.js
@@ -11,15 +11,78 @@
 // //console.log('Hello World... Just a test')
 
 
+
 // function myName() {
 //     console.log("Moh Kam");
 // }
 // myName();
 
 
-//function that returns the person name 
-function nameHi(name) {
-    return "Hello " + name;
+
+// //function that returns the person name 
+// function nameHi(name) {
+//     return "Hello " + name;
+// }
+// let n = nameHi("momo");
+// console.log(n);
+
+
+
+// //pass function within a function
+// function printVar(v) {
+//     console.log(v);
+// }
+
+// function func(x) {
+//     console.log("before printing...");
+//     x("hello world");
+//     console.log(".. after printing");
+// }
+
+// func(printVar);
+
+
+
+// //Callbacks are very commonly used in JS
+// function sumCallback(a, b, callback) {
+//     callback(a, b);
+// }
+
+// function callback(a, b) {
+//     console.log(a + b);
+// }
+
+// sumCallback(1, 2, callback);
+
+
+
+// Create a new function that takes 2 parameters.
+// 1. name,
+// 2. callback, that prints out what we pass to it...
+// take the name and append "hello" to the beginning of name
+function printVar(x) {
+    console.log(x);
 }
-let n = nameHi("momo");
-console.log(n);
+
+function printName(name, callback) {
+    callback("Hello " + name);
+}
+printName("Meow", printVar);
+
+// ******* This was my soln...
+// function printVar(name, callback) {
+//     let x = callback(name);
+//     console.log(x);
+// }
+
+// function myCallback(myName) {
+//     return "Hello " + myName;
+// }
+
+// printVar("Mooo", myCallback);
+
+
+
+
+
+
