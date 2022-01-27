5b8e50f4ce0150a824842a9e18d2cd3fa0dae43d
mo <mohibullah.kamal@gmail.com>
2022-01-28 05:42:48 +0600

    add router to express server

--- node/expressWDSimplified/server.js
+++ node/expressWDSimplified/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 2000;
+
+app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-    console.log('Server running on PORT->' + port);
-    res.send("Hi... from server");
+    console.log('Server running on PORT-->' + port);
+    // res.send("Hi... Hi... from server");
+    // res.json({ massage: 'Error' })
+    res.render("./views/index.ejs", { text: "World" })
 })
 
+const userRouter = require('./routes/users')
+
 app.listen(port);
+
+
+
