539207457f6d3e62ebb3c2500e70dfca677f1508
mo <mohibullah.kamal@gmail.com>
2022-02-16 23:07:05 +0600

    localStorage done

--- reactjs/learn-react/learn-reacttt/src/App.js
+++ reactjs/learn-react/learn-reacttt/src/App.js
@@ -1,36 +1,54 @@
 import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./TodoList";
-import uuid from 'uuid/v4'
+import uuidv4 from "uuid/v4";
 
 const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   // const [listtt, setTodos] = useState(['data1', 'data2'])
   // const [listtt, setTodos] = useState([{ id: 1, name: 'todo1', complete: true }])
-  const [listtt, setTodos] = useState([])
+  const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
 
   useEffect(() => {
-    localStorage.setItem()
-  }, [listtt])
+    const storedTodos = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
     if (name === '') return
     // console.log(name)
     setTodos(prevTodos => {
-      return [...prevTodos, { id: uuid(), name: name, complete: false }]
+      // return [...prevTodos, { id: uuidv4, name: name, complete: false }]
+      return [...prevTodos, { id: 1, name: name, complete: false }]
     })
     todoNameRef.current.value = null
   }
 
+  function handleClearTodos(e) {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
     <>
-      <TodoList todos={listtt} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Completed</button>
-      <div>0 left to do</div>
+      <button onClick={handleClearTodos}>Clear Completed</button>
+      <div>{todos.filter(todo -> !todo.complete).length} left to do</div>
 
     </>
   )
